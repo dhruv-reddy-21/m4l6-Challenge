ea756f1980d0f775608e25c1578f11ade4594fc6
Dhruv Reddy <dhruv.reddy700@gmail.com>
2022-01-19 16:12:55 -0800

    Initial Commit

---
+++ m4l6 Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  m4l6 Challenge
+//
+//  Created by Dhruv Reddy on 1/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ m4l6 Challenge/m4l6_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  m4l6_ChallengeApp.swift
+//  m4l6 Challenge
+//
+//  Created by Dhruv Reddy on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct m4l6_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
